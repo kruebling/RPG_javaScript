32834d03f31e44246d55bdded280de08d2e01baa
Dawson Mortenson and Keegan Ruebling <keegan@>
2017-10-11 13:50:07 -0700

    add movement function to living class

--- js/rpg.js
+++ js/rpg.js
@@ -1,21 +1,18 @@
 export class Env  {
-  constructor() {
-    this.map = new Array(5);
-    for (let i = 0; i < 5; i++) {
+  constructor(size) {
+    this.size = size;
+    this.map = new Array(this.size);
+    for (let i = 0; i < this.size; i++) {
       this.map[i] = [0,0,0,0,0];
     }
   }
 
-  moveValidator(y, x, player_y, player_x){
-    if((y === (player_y + 1)) || (y === (player_y - 1)) || (x === (player_x + 1)) || (x === (player_x - 1))) {
-      return true;
-    }
-    return false;
-  }
+
 }
 
 export class Living  {
-  constructor(lvl,health,mana,strength,x,y) {
+  constructor(name,lvl,health,mana,strength,x,y) {
+    this.name = name;
     this.lvl = lvl;
     this.exp = 0;
     this.health = health;
@@ -27,16 +24,30 @@ export class Living  {
     // this.spells = [];
   }
 
+  move(y, x, an_env){
+    if ((y < an_env.size && y >= 0) && (x < an_env.size && x >= 0)){
+      if ((y === this.pos_y) && (x === (this.pos_x + 1)) || (y === this.pos_y) && (x === (this.pos_x - 1)) || (x === this.pos_x) && (y === (this.pos_y + 1)) || (x === this.pos_x) && (y === (this.pos_y - 1))) {
+        // Transistion position on map in an_env object
+        an_env.map[this.pos_y][this.pos_x] = 0;
+        an_env.map[y][x] = this;
+        // Updates the position of the living object
+        this.pos_y = y;
+        this.pos_x = x;
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 export class Player extends Living {
-  constructor() {
-    super(1,100,50,10,0,0);
+  constructor(name) {
+    super(name,1,100,50,10,0,0);
   }
 }
 
 export class Enemy extends Living {
-  constructor(lvl,health,strength,x,y) {
-    super(lvl,health,0,strength,x,y);
+  constructor(name,lvl,health,strength,x,y) {
+    super(name,lvl,health,0,strength,x,y);
   }
 }
